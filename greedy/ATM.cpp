e47963e0c180cf7d0a7810bfe1f8e4d37dd184ef
wonone17 <38302837+wonone17@users.noreply.github.com>
2019-01-18 05:39:11 +0900

    Add files via upload

--- greedy/ATM.cpp
+++ greedy/ATM.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 int main() {
-	int num,tmp,sum=0;
+	int num, tmp, sum = 0;
 	vector<int> t;
 	cin >> num;
-	while (num--){
+	while (num--) {
 		cin >> tmp;
 		t.push_back(tmp);
 	}
 	sort(t.begin(), t.end());
 	for (int i = 0; i < t.size(); i++) {
-		sum += (t.size()-i)*t[i];
+		sum += (t.size() - i)*t[i];
 	}
 	cout << sum;
 }
