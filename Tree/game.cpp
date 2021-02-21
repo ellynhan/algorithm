a813f52ab0ab1de1f3a25e9b9a41403860e6144f
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2021-02-21 13:50:22 +0900

    백준 1103번 게임 dfs를 dp로 풀기.

--- Tree/game.cpp
+++ Tree/game.cpp
@@ -1,46 +1,44 @@
-//12시 4분 시작 30분 종료
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-//숫자보고 방향고르고 고른방향으로 숫자만큼움직임. 중간에 구멍은 무시
-//구멍에 빠지거나 보드 바깥으로 가면 게임 종료
 int N, M; //세로 가로
 char board[50][50];
+
 //상하좌우
 int xx[4] = {0,0,-1,1};
 int yy[4] = {1,-1,0,0};
-int visited[50][50]={0,}; // 1나왔던 곳에 도착하면 -1
-int maxValue = 0;
 
-int dfs(pair<int, int> root, int depth){
-    for(int i=0; i<4; i++){
-        int value = board[root.first][root.second]-'0';
-        int x = root.second + xx[i]*value;
-        int y = root.first + yy[i]*value;
-        if(x<0 || y<0 || x>=M || y>=N || board[y][x]=='H'){
-            if(maxValue < depth){
-                maxValue = depth;
-            }
-        }
-        else{
-            if(visited[y][x] == 1){
-                return -1;
-            }else{
-                visited[y][x] = 1;
-                if(dfs({y,x}, depth + 1) == -1){
-                    return -1;
-                }
-                visited[y][x] = 0;
-            }
-            
-        }
+int visited[50][50]={0,}; 
+
+int dp[50][50]={0,};
+
+int dfs(pair<int, int> root){
+    int x = root.second;
+    int y = root.first;
+    if(x<0 || y<0 || x>=M || y>=N || board[y][x]=='H'){
+        return 0;
     }
-    return 0;
+    else if(visited[y][x]==1){
+        cout<<-1;
+        exit(0);
+    }else if(dp[y][x]==0){
+        int value = board[y][x]-'0';
+        visited[y][x]=1;
+        for(int i=0; i<4; i++){
+            dp[y][x] = max(dp[y][x],dfs({y+yy[i]*value,x+xx[i]*value})+1);
+        }
+        visited[y][x]=0;
+    }
+    return dp[root.first][root.second];
 }
 
+
 int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+
     cin >>N>>M;
-    int result;
     for(int i=0; i<N; i++){
         string str;
         cin >> str;
@@ -48,11 +46,5 @@ int main(){
             board[i][j] = str[j];
         }
     }
-    visited[0][0] = 1;
-    result = dfs({0,0},1);
-    if(result == -1){
-        cout<< -1;
-    }else{
-        cout<<maxValue;
-    }
+    cout<< dfs({0,0});
 }
