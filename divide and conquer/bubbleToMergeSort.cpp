fbb1095a4e5dc948a490d2fe52b7333ebab614c1
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2021-03-15 00:33:57 +0900

    백준 1517번 버블정렬 수정해서 맞춤 근데 이유 알아낼 것.

--- divide and conquer/bubbleToMergeSort.cpp
+++ divide and conquer/bubbleToMergeSort.cpp
@@ -47,3 +47,49 @@ int main(){
     cout<<cnt;
     
 }
+// 밑에 맞은 코드와 비교해서 정답 이유 알아 낼 것.!!!!!!!!!
+#include <iostream>
+using namespace std;
+long long cnt =0;
+void merge(long long* A,int begin, int mid, int end, long long* B){
+    int i,j,k;
+    i=begin;
+    j=mid;
+    for(k=begin; k<=end; k++){
+        if (i<mid&&(j>end||A[i]<=A[j])) {
+            B[k] = A[i];
+            i = i + 1;
+        } else {
+            cnt += k>j ? k-j : j-k;
+            B[k] = A[j];
+            j = j + 1;
+        }
+    }
+    for(k=begin; k<=end; k++){ // update
+        A[k]=B[k];
+    }
+}
+
+void split(long long* A, int begin, int end, long long* B){
+    int mid;
+    
+    if(end-begin<1) return; //두 조각 남음
+    mid=begin+(end-begin)/2;
+    split(A,begin,mid,B);
+    split(A,mid+1,end,B);
+    merge(A,begin,mid+1,end,B);
+}
+
+
+
+int main(){
+    int N; // 수열의 길이
+    cin>>N;
+    long long* A = new long long[N];
+    long long* B = new long long[N];
+    for(int i=0; i<N; i++){
+        cin>>A[i];
+    }
+    split(A,0,N-1,B);
+    cout<<cnt;
+}
