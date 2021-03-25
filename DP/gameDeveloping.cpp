93cf6534c2500acb1e7926e92f456a4e3ea5a831
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2021-03-25 20:32:17 +0900

    백준 1516번: 게임 개발
    DP도 사용하긴 했지만 위상 정렬 개념이 핵심이라 그래프에 가깝다고 생각함. 근데 아직 이해가 완전히 되지는 않았음.

--- DP/gameDeveloping.cpp
+++ DP/gameDeveloping.cpp
@@ -1,36 +1,49 @@
-//0558
 #include <iostream>
+#include <queue>
 using namespace std;
 
-int dp[501]={0,};
-int timeAndPrev[501][2]={0,};
-
-void build(int i){
-    int prev = timeAndPrev[i][1];
-    if(dp[prev]==0&&prev!=0){
-        build(prev);
-    }
-    dp[i] = dp[prev] + timeAndPrev[i][0];
-}
+int numOfParents[501]={0,};
+queue<int> children[501];
+int buildTimes[501]={0,};
+int totalTimes[501]={0,}; //dp
 
 int main(){
-    int N;
-    cin >> N;
-
-    for(int i=1; i<=N; i++){
-        int value, j=0;
-        cin >> value;
-        while(value!=-1){
-            timeAndPrev[i][j]=value;
-            j++;
-            cin >> value;
+    int n;
+    cin >> n;
+    queue<int> noParents;
+    for(int i=1; i<=n; i++){
+        cin >> buildTimes[i];
+        int parent;
+        cin >> parent;
+        int cnt = 0;
+        while(parent!=-1){
+            cnt ++;
+            children[parent].push(i);
+            numOfParents[i]++;
+            cin >> parent;
+        }
+        if(cnt==0){
+            noParents.push(i);
         }
     }
 
-    for(int i=1; i<=N; i++){
-        if(dp[i]==0){
-            build(i);
+    while(!noParents.empty()){
+        int node = noParents.front();
+        totalTimes[node]+=buildTimes[node];
+        while(!children[node].empty()){
+            int child = children[node].front();
+            children[node].pop();
+            numOfParents[child]--;
+            totalTimes[child]=max(totalTimes[node],totalTimes[child]); //이 부분이 핵심.. 
+            if(numOfParents[child]==0){
+                noParents.push(child);
+            }
         }
-        cout<<dp[i]<<endl;
+        noParents.pop();
+    }
+
+    for(int i=1; i<=n; i++){
+        cout<<totalTimes[i];
+        if(i!=n)cout<<endl;
     }
 }
