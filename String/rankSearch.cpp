1e130886065910bd18f3034413fcd0ded5c12544
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2021-05-06 13:31:49 +0900

    2021 KAKAO BLIND RECRUITMENT
    이전에 효율성문제 부분이 문제였는데, 이부분을 4차원 배열형태를 만들어서 범위를 나눌 수 있도록하여 분할 정복으로 해결한 문제이다.

--- String/rankSearch.cpp
+++ String/rankSearch.cpp
@@ -1,4 +1,3 @@
-//0419
 #include <string>
 #include <vector>
 #include <set>
@@ -6,65 +5,132 @@
 #include <sstream>
 #include <iostream>
 using namespace std;
+
+vector<int> db[3][2][2][2];
+
+void divide1(string str){
+    int pos = str.find(" ");
+    vector<string> v;
+    int cv[4];
+    while(pos!=string::npos){
+        v.push_back(str.substr(0, pos));
+        str = str.substr(pos+1,str.length()-pos);
+        pos = str.find(" ");
+    }
+    if(v[0]=="cpp"){
+        cv[0]=0;
+    }else if(v[0]=="java"){
+        cv[0]=1;
+    }else{
+        cv[0]=2;
+    }
+
+    if(v[1]=="backend"){
+        cv[1]=0;
+    }else{
+        cv[1]=1;
+    }
+
+    if(v[2]=="junior"){
+        cv[2]=0;
+    }else{
+        cv[2]=1;
+    }
+
+    if(v[3]=="chicken"){
+        cv[3]=0;
+    }else{
+        cv[3]=1;
+    }
+    db[cv[0]][cv[1]][cv[2]][cv[3]].push_back(stoi(str));
+}
+
+vector<int> divide2(string str){
+    int pos = str.find(" and ");
+    vector<string> v;
+    vector<int> cv;
+    cv.resize(5);
+    while(pos!=string::npos){
+        v.push_back(str.substr(0, pos));
+        str = str.substr(pos+5,str.length()-pos-4);
+        pos = str.find(" and ");
+    }
+    while(str[pos]!=' '){
+        pos++;
+    }
+    v.push_back(str.substr(0, pos));
+    str = str.substr(pos+1,str.length()-pos);
+    if(v[0]=="-"){
+        cv[0]=-1;
+    }else if(v[0]=="cpp"){
+        cv[0]=0;
+    }else if(v[0]=="java"){
+        cv[0]=1;
+    }else{
+        cv[0]=2;
+    }
+
+    if(v[1]=="-"){
+        cv[1]=-1;
+    }else if(v[1]=="backend"){
+        cv[1]=0;
+    }else{
+        cv[1]=1;
+    }
+
+    if(v[2]=="-"){
+        cv[2]=-1;
+    }else if(v[2]=="junior"){
+        cv[2]=0;
+    }else{
+        cv[2]=1;
+    }
+
+    if(v[3]=="-"){
+        cv[3]=-1;
+    }else if(v[3]=="chicken"){
+        cv[3]=0;
+    }else{
+        cv[3]=1;
+    }
+    cv[4]=stoi(str);
+    return cv;
+}
+
 vector<int> solution(vector<string> info, vector<string> query) {
-    //개발언어 직군 경력 소울푸드 점수 50000개 이하
     vector<int> answer;
-    map<string, vector<int>> m;
-    vector<int> score;
+    
     for(int i=0; i<info.size(); i++){
-        istringstream buf(info[i]);
-        string word;
-        for(int j=0; j<4; j++){
-            buf >> word;
-            if(m.count(word)>0){
-                m[word].push_back(i);
-            }else{
-                vector<int> v = {i};
-                m[word]=v;
-            }
-        }
-        buf >> word;
-        score.push_back(stoi(word));
+        divide1(info[i]);
     }
     
     for(int i=0; i<query.size(); i++){
+        vector<int> q = divide2(query[i]);
+        int ja=0,jb=2,ka=0,kb=1,ma=0,mb=1,na=0,nb=1;
+        if(q[0]!=-1)ja=jb=q[0];
+        if(q[1]!=-1)ka=kb=q[1];
+        if(q[2]!=-1)ma=mb=q[2];
+        if(q[3]!=-1)na=nb=q[3];
         int total = 0;
-        istringstream buf(query[i]);
-        string word;
-        map<int, int> check;
-        for(int j=0; j<info.size(); j++){
-            check[j]=0;
-        }
-        int checkNum = 4;
-        for(int j=0; j<4; j++){
-            buf >> word;
-            if(word=="-"){
-                checkNum --;
-            }else{
-                if(m[word].size()==0){
-                    break;
-                }else{
-                    for(int k=0; k<m[word].size(); k++){
-                        check[m[word][k]] ++;
+        for(int j=ja; j<=jb; j++){
+            for(int k=ka; k<=kb; k++){
+                for(int m=ma; m<=mb; m++){
+                    for(int n=na; n<=nb; n++){
+                        for(int l=0; l<db[j][k][m][n].size(); l++){
+                            if(db[j][k][m][n][l]>=q[4]){
+                                total ++;
+                            }   
+                        }
                     }
                 }
             }
-            buf >> word;
-        }
-        buf >> word;
-        map<int, int>::iterator it;
-        for(it=check.begin(); it!=check.end(); it++){
-            if(it->second == checkNum && score[it->first]>=stoi(word)){
-                total++;
-            }
-            
         }
         answer.push_back(total);
     }
     return answer;
 }
 
+
 int main(){
     solution({"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"},{"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"});
-
 }
