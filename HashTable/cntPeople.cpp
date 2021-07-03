6e27a864e751ed253937f2acea084c8dca84789f
ellynhan <wown252@naver.com>
2021-07-03 13:19:19 +0900

    백준 7785번: 회사에 있는 사람(해쉬테이블)

--- HashTable/cntPeople.cpp
+++ HashTable/cntPeople.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 
 int main() {
-	int T; cin >> T;
+	int len; cin >> len;
 	set<string> s;
-	while (T--) {
-		string a, b;
-		cin >> a >> b;
-		if (b == "enter")s.insert(a);
-		else s.erase(a);
+	while (len--) {
+		string name, state;
+		cin >> name >> state;
+		if (state == "enter")s.insert(name);
+		else s.erase(name);
 	}
 	for (auto it = s.rbegin(); it != s.rend(); it++)
 		cout << *it << "\n";
 	return 0;
 }
+
+//해쉬 이용해서 다시풀기. 
