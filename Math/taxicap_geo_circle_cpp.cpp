7789a5cd8c24e22f9205deb3e9b7a9bfac43f970
ellynhan <wown252@naver.com>
2021-07-15 20:00:32 +0900

    백준 3053번: 택시기하학

---
+++ Math/taxicap_geo_circle_cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int r ;
+    cin >> r;
+    cout << fixed;
+    cout.precision(6);
+    cout<<r*r*M_PI<<'\n';
+    cout<<double(2*r*r);
+}
